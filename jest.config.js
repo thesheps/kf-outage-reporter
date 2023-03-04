ad038f41ce8bfa2d5f1b69fe9424e53058e7353f
Sheps <thesheps@gmail.com>
2023-03-04 09:22:29 +0000

    test(): adds jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: "test-report/coverage",
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Outage Reporter Test Results",
+        outputPath: "./test-report/index.html",
+        sort: "titleAsc"
+      }
+    ]
+  ],
 };
